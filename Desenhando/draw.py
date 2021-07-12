b9ecf73017bbabc935804638f5bbe6cfa8a8030e
João Pedro Cherem <86925982+jaocherem@users.noreply.github.com>
2021-07-12 20:16:50 -0300

    Add files via upload

---
+++ Desenhando/draw.py
@@ -0,0 +1,19 @@
+import turtle
+
+cock = turtle.Turtle()
+
+cock.width(5)
+cock.color('purple')
+
+cock.penup()
+cock.goto(0, -150)
+cock.pendown()
+
+cock.right(45)
+cock.circle(60, 225)
+cock.right(90)
+cock.forward(200)
+cock.circle(50, 180)
+cock.forward(200)
+cock.right(90)
+cock.circle(60, 225)
